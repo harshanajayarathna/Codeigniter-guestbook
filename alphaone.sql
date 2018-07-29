0117f9d7cc1a6b663169be49b2b2cc20bc1e96a0
harshanajayarathna <harshanajayarathna@gmail.com>
2018-07-30 11:38:10 +1200

    add new data to database

--- alphaone.sql
+++ alphaone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 30, 2018 at 12:27 AM
+-- Generation Time: Jul 30, 2018 at 01:37 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 5.6.33
 
@@ -44,24 +44,17 @@ CREATE TABLE `guestbook` (
 --
 
 INSERT INTO `guestbook` (`id`, `user_name`, `user_email`, `user_address`, `user_message`, `user_ip`, `user_os`, `user_browser`) VALUES
-(1, 'Harshana', 'harshanajayarathna@gmail.com', '10 King Street, Palmerston North, New Zealand', 'This is first test message.', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(2, 'Umani', 'umanishanika@gmail.com', '10 King Street Palmerston north, New Zealand', 'This is the second message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(3, 'Ruchira', 'ruchir@gmail.com', '163, Anuradhapura Road, Galenbindunuwewa, Sri Lanka', 'test third message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(4, 'Dhanushka', 'dhanushka@gmail.com', '123, king Street, Palmerston North, New Zealand', 'This message was written by Dhanushka', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(5, 'John', 'john@gmail.com', '10 King Street, PN, NZ', 'This message was written by John', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(6, 'xcv', 'harshanajayarathna@gmail.com', '10 King Street', 'xcv', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(7, 'Harshana 1', 'harshanajayarathna@gmail.com', '10 King Street', 'xgvsdgsdgsdg', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(8, 'Harshana 5', 'harshanajayarathna@gmail.com', '10 King Street', 'zzz vbnm hjkl;', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(9, 'Harshana 6', 'harshanajayarathna@gmail.com', '10 King Street', 'zxvxgdg', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(10, 'Harshana 7', 'harshanajayarathna@gmail.com', '10 King Street', 'dfdsf dsfdsfsdf dfssdfds', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(11, 'Harshana', 'harshanajayarathna@gmail.com', '10 King Street', '1233444', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(12, 'Harshana', 'harshanajayarathna@gmail.com', '10 King Street', 'dsf', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(13, 'Harshana gh', 'harshanajayarathna@gmail.com', '10 King Street', 'fgfdgdg', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(14, 'abc', 'harshanajayarathna@gmail.com', '10 King Street', 'abc', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(15, 'sdada', 'harshanajayarathna@gmail.com', '10 King Street', 'asdfadsasd', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(16, 'aaaaaaaaaa', 'harshanajayarathna@gmail.com', '10 King Street', 'aaaaaaaaaaaaa', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(17, 'zzzzz', 'harshanajayarathna@gmail.com', '10 King Street', 'zzzzzz zzzzzzzzzzz', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
-(18, 'wedsa', 'harshanajayarathna@gmail.com', '10 King Street', 'as', '::1', 'Windows 10', 'Chrome 67.0.3396.99');
+(1, 'Sir Don Bradman', 'bradman@gmail.com', 'Australia', 'This is Bradman\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(2, 'Sachin Tendulkar', 'sachin@yahoo.com', 'Mumbai, India', 'This is Sachin\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(3, 'Gary Sobers', 'sobers@gmail.com', 'Bridgetown, Barbados', 'This is Gary\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(4, 'Vivians Richard', 'richard@hotmail.com', 'Antigua and Barbuda', 'This is Richard\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(5, 'Kapil Dev', 'kapil@gmail.com', 'Chandigarh, India', 'This is Kapil\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(6, 'Imran Khan', 'imran@yahoo.com', 'Lahore, Pakistan', 'This is Imran\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(7, 'Ian Botham', 'ian@gmail.com', 'Cheshire, United Kingdom', 'This is Ion\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(8, 'Dennis Lillee', 'dennis@yahoo.com', 'Subiaco, Australia', 'This is Dennis\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(9, 'Jacques Kallis', 'kallis@gmail.com', 'Pinelands, Cape Town, South Africa', 'This is Kallis message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(10, 'Muttiah Muralitharan', 'muttiah@gmail.com', 'Kandy, Sri Lanka', 'This is Muttiah\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99'),
+(11, 'Brian Lara', 'lara@gmail.com', 'Santa Cruz, Trinidad and Tobago', 'This is Brian Lara\'s message', '::1', 'Windows 10', 'Chrome 67.0.3396.99');
 
 --
 -- Indexes for dumped tables
@@ -81,7 +74,7 @@ ALTER TABLE `guestbook`
 -- AUTO_INCREMENT for table `guestbook`
 --
 ALTER TABLE `guestbook`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
